04a7e409a9902bed03214de7d7b4a3e346d24f91
sipk <hiroshige.kawaguchi@gmail.com>
2021-07-31 10:55:17 +0900

    Add: clear ボタンを追加

--- src/components/Problem.vue
+++ src/components/Problem.vue
@@ -1,17 +1,69 @@
 <template>
   <div class="problem">
     <input class="problem__formula"
+        v-model="formula"
         type="text" 
-        placeholder="32+48"
+        placeholder="12+34"
       >
+    <button class="problem__clear"
+        @click="clear"
+      >
+      <i class="las la-backspace"
+          :class="iconStyle"
+        >
+      </i>
+    </button>
   </div>
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+      formula: '',
+    }
+  },
+  computed: {
+    iconStyle () {
+      return 'on-color';
+    }
+  },
+  methods: {
+    clear () {
+      this.formula = '';
+    }
+  }
+}
+</script>
+
 <style scoped>
-  .problem {}
+  .on-color {
+    color: #ff6150;
+    opacity: 1;
+  }
+
+  .problem {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
     .problem__formula {
       color: #134e6f;
       font-size: 60px;
+      width: 70%;
+
       text-align: center;
+    }
+    .problem__clear {
+      background-color: #ff615033;
+      font-size: 32px;
+      width: 66px; height: 66px;
+      margin-left: 24px;
+      border-radius: 50%;
+      border: none;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
     }
 </style>
